c5d1283db0dadb97192f609392d58e16da026133
TED Vortex <ted.vortex@gmail.com>
2016-05-30 21:10:21 +0100

    Fixed skill list display according to jsonresume.org schema

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,14 +4,21 @@ export default class Skills extends React.Component {
 
   render() {
 
-    let getSkills = this.props.skillsData[0].keywords.map(function(item) {
-      return (<li><span className="label label-success">{item}</span></li>)
-    });
-
   	return (
   	  <section className="skills">
         <h2 className="text-uppercase"><i className="fa fa-lg fa-code"></i> Skills</h2>
-        <ul className="skills-list list-inline">{getSkills}</ul>
+        {this.props.skillsData.map(function(item) {
+          return (
+            <div>
+              <h3>{item.name}</h3>
+              <ul className="skills-list list-inline">{item.keywords.map(function(skill) {
+                return (
+                  <li><span className="label label-success">{skill}</span></li>
+                )
+              })}</ul>
+            </div>
+          )
+        })}
       </section>
   	)
 
